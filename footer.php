f5f0aeb938f0afa313adafba39348313a5bca6dd
AnvitK <anvitkamble@gmail.com>
2020-11-30 15:24:28 +0530

    Initial commit

---
+++ footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer_style.css">
+</head>
+<body>
+    <footer class="footer"> 
+               <div class="container">
+               <center>
+                   <p>Copyright &copy SAS-Bank. All Rights Reserved.</p>
+                   <p>This website is developed by SAS group</p>
+               </center>
+               </div>
+    </footer>
+</body>
+</html>
